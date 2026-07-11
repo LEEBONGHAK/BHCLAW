1f31766a56567dc9ba35ef7c13539e7968484f75
LEEBONGHAK <dlqhdgkr456@gmail.com>
2026-07-11 16:34:21 +0900

    2.5 Concurrency

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -1,25 +1,28 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePotato extends DurableObject<Env> {
+	sql: SqlStorage;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
+		this.sql = ctx.storage.sql;
 
-		ctx.storage.sql.exec(`
+		this.sql.exec(`
 			CREATE TABLE IF NOT EXISTS pongs (
-				id INTEGER PRIMARY KEY AUTOINCREMENET
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				total INTEGER
 			)
 		`);
 
-		ctx.storage.sql.exec(`
+		this.sql.exec(`
 			INSERT OR IGNORE INTO pongs (id, total) VALUES (1, 0);
 		`);
 	}
 
-	count = 0;
 	increase() {
-		this.count++;
-		return `count is ${this.count}`;
+		const { total } = this.sql.exec(`SELECT total FROM pongs`).one() as { total: number };
+		this.sql.exec(`UPDATE pongs SET total = ? WHERE id = ?`, total + 1, 1);
+		return `count is ${total + 1}`;
 	}
 
 	ping() {
