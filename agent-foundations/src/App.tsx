44c8a168c09e28ca724ab1c7870b05d45a545bac
LEEBONGHAK <dlqhdgkr456@gmail.com>
2026-07-12 14:05:55 +0900

    3.3 Messages

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -1,24 +1,51 @@
 import { useAgent } from "agents/react";
-import type { ChattingRoomAgent, PingPongState } from "../worker/index";
+import type { ChattingRoomAgent, ChattingRoomState } from "../worker/index";
 import { useState } from "react";
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
   //   const [pingPongs, setPingPongs] = useState(0);
-  const agent = useAgent<ChattingRoomAgent, PingPongState>({
+  const [message, setMessage] = useState("");
+  const agent = useAgent<ChattingRoomAgent, ChattingRoomState>({
     agent: "ChattingRoomAgent",
     onOpen: () => setIsConnected(true),
     // onStateUpdate: (state) => setPingPongs(state.pingPongCount),
+    onMessage: (event) => console.log(event),
   });
 
+  const sendMessage = () => {
+    agent.send(message); // agnet를 직접 사용해 메시지를 웹소켓으로 보냄.
+    setMessage("");
+  };
+
   if (!isConnected) return <h1>connecting...</h1>;
+
   return (
     <div>
-      <h1>Ping Pong Agent</h1>
-      <h3>Ping Pong Count: {agent.state?.pingPongCount}</h3>
+      <h1>Chatting Room Agent</h1>
+      <h3>Online people: {agent?.state?.currentlyOnline}</h3>
       <hr />
-      <button onClick={() => agent.stub.decrement()}>decrement</button>
+      {/* <button onClick={() => agent.stub.decrement()}>decrement</button>
       <button onClick={() => agent.stub.increment()}>increment</button>
+      <button onClick={() => agent.setState({ currentlyOnline: 100000 })}>
+        override
+      </button> */}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage();
+        }}
+      >
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+          autoFocus
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,26 +1,70 @@
-import { Agent, callable, routeAgentRequest } from "agents";
+import {
+  Agent,
+  callable,
+  routeAgentRequest,
+  type Connection,
+  type ConnectionContext,
+  type WSMessage,
+} from "agents";
 
-export type PingPongState = {
-  pingPongCount: number;
+export type ChattingRoomState = {
+  currentlyOnline: number;
 };
 
-export class ChattingRoomAgent extends Agent<Env, PingPongState> {
+export class ChattingRoomAgent extends Agent<Env, ChattingRoomState> {
   initialState = {
-    pingPongCount: 0,
+    currentlyOnline: 0,
   };
 
   @callable() // 프런트엔드에서 호출할 수 있게 만들어줌
   increment() {
     this.setState({
-      pingPongCount: this.state.pingPongCount + 1,
+      currentlyOnline: this.state.currentlyOnline + 1,
     });
   }
 
   @callable()
   decrement() {
     this.setState({
-      pingPongCount: this.state.pingPongCount - 1,
+      currentlyOnline: this.state.currentlyOnline - 1,
     });
+  }
+
+  // state 변경 감지 내장 메서드
+  onStateChanged(
+    _state: ChattingRoomState | undefined,
+    _source: Connection | "server",
+  ): void {
+    console.log("new state", _state);
+    console.log("who did it", _source);
+  }
+
+  // 연결 감지 내장 메서드
+  onConnect(
+    connection: Connection,
+    ctx: ConnectionContext,
+  ): void | Promise<void> {
+    this.setState({
+      currentlyOnline: this.state.currentlyOnline + 1,
+    });
+  }
+
+  // 연결 종료 감지 내장 메서드
+  onClose(
+    connection: Connection,
+    code: number,
+    reason: string,
+    wasClean: boolean,
+  ): void | Promise<void> {
+    this.setState({
+      currentlyOnline: this.state.currentlyOnline - 1,
+    });
+  }
+
+  // 메시지 감지 내장 메서드
+  onMessage(connection: Connection, message: WSMessage): void | Promise<void> {
+    console.log(message);
+    connection.send("love you back"); // 서버에서 연결된 상대에게 메시지 발송
   }
 }
 
