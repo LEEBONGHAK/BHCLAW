4232cacb008b8f70b9612b64fb4f1d421d334c04
LEEBONGHAK <dlqhdgkr456@gmail.com>
2026-07-12 13:11:14 +0900

    3.1 AgentState

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -1,5 +1,21 @@
+import { useAgent } from "agents/react";
+import type { ChattingRoomAgent, PingPongState } from "../worker/index";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [isConnected, setIsConnected] = useState(false);
+  const agent = useAgent<ChattingRoomAgent, PingPongState>({
+    agent: "ChattingRoomAgent",
+    onOpen: () => setIsConnected(true),
+  });
+
+  if (!isConnected) return <h1>connecting...</h1>;
+  return (
+    <div>
+      <h1>Ping Pong Agent</h1>
+      <h3>Ping Pong Count: {agent.state?.pingPongCount}</h3>
+    </div>
+  );
 }
 
 export default App;
--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,9 +1,30 @@
 import { Agent, routeAgentRequest } from "agents";
 
-export class ChattingRoomAgent extends Agent<Env> {}
+export type PingPongState = {
+  pingPongCount: number;
+};
+
+export class ChattingRoomAgent extends Agent<Env, PingPongState> {
+  initialState = {
+    pingPongCount: 0,
+  };
+
+  increment() {
+    this.setState({
+      pingPongCount: this.state.pingPongCount + 1,
+    });
+  }
+
+  decrement() {
+    this.setState({
+      pingPongCount: this.state.pingPongCount - 1,
+    });
+  }
+}
 
 export default {
   async fetch(request, env) {
+    console.log(request.url);
     const agentResponse = await routeAgentRequest(request, env);
     if (agentResponse) return agentResponse;
     return new Response(null, { status: 404 });
