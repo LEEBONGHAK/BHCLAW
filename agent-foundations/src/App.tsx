46ec1ed0e35ec5fe822a0f3a5c1f4e5a1ddf5268
LEEBONGHAK <dlqhdgkr456@gmail.com>
2026-07-12 13:34:57 +0900

    3.2 Callables

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
+  //   const [pingPongs, setPingPongs] = useState(0);
   const agent = useAgent<ChattingRoomAgent, PingPongState>({
     agent: "ChattingRoomAgent",
     onOpen: () => setIsConnected(true),
+    // onStateUpdate: (state) => setPingPongs(state.pingPongCount),
   });
 
   if (!isConnected) return <h1>connecting...</h1>;
@@ -14,6 +16,9 @@ function App() {
     <div>
       <h1>Ping Pong Agent</h1>
       <h3>Ping Pong Count: {agent.state?.pingPongCount}</h3>
+      <hr />
+      <button onClick={() => agent.stub.decrement()}>decrement</button>
+      <button onClick={() => agent.stub.increment()}>increment</button>
     </div>
   );
 }
--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,4 +1,4 @@
-import { Agent, routeAgentRequest } from "agents";
+import { Agent, callable, routeAgentRequest } from "agents";
 
 export type PingPongState = {
   pingPongCount: number;
@@ -9,12 +9,14 @@ export class ChattingRoomAgent extends Agent<Env, PingPongState> {
     pingPongCount: 0,
   };
 
+  @callable() // 프런트엔드에서 호출할 수 있게 만들어줌
   increment() {
     this.setState({
       pingPongCount: this.state.pingPongCount + 1,
     });
   }
 
+  @callable()
   decrement() {
     this.setState({
       pingPongCount: this.state.pingPongCount - 1,
