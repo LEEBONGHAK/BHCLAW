e3d0d550854f2bc15353b0790f6b847708f2115e
LEEBONGHAK <dlqhdgkr456@gmail.com>
2026-07-26 19:02:40 +0900

    4.7 sanitize messages

--- ai-chat-agent-foundations/worker/index.ts
+++ ai-chat-agent-foundations/worker/index.ts
@@ -7,6 +7,7 @@ import {
   streamText,
   type GenerateTextOnFinishCallback,
   type ToolSet,
+  type UIMessage,
 } from "ai";
 import { createWorkersAI } from "workers-ai-provider";
 import { buyPlaneTicket, getLocation, getTickets, getWeather } from "./tools";
@@ -50,6 +51,23 @@ export class PotatoChatAgent extends AIChatAgent<Env> {
     });
 
     return result.toUIMessageStreamResponse();
+  }
+
+  // 내용을 바꾸거나 검열하고 싶을 때 사용 가능, 메시지가 저장될 때마다 호출되고 메시지를 수정할 수	있게 되며 꼭 return 해야함.
+  // 프롬프트 내 내용이 검열되는 것 뿐이지 대답은 정상적으로 진행됨.
+  protected sanitizeMessageForPersistence(message: UIMessage): UIMessage {
+    return {
+      ...message,
+      parts: message.parts.map((part) => {
+        if (part.type === "text") {
+          return {
+            ...part,
+            text: part.text.replace("food", "X stop eating u fat X"),
+          };
+        }
+        return part;
+      }),
+    };
   }
 }
 
